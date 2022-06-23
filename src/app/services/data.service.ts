82069a5812e549881bcdca30ac79d480a5b1ab78
zalbrech <albrecht.zachary1@gmail.com>
2022-06-23 18:26:37 -0500

    added [state,abbreviation] map to DataService

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,28 +12,68 @@ export class DataService {
   
   constructor() { 
     this.twoLetterStates = new Set<string>([
-      'AL', 'AK', 'AZ', 'AR', 
-      'CA', 'CO', 'CT', 'DE', 
-      'FL', 'GA', 'HI', 'ID',
-      'IL', 'IN', 'IA', 'KS',
-      'KY', 'LA', 'ME', 'MD',
-      'MA', 'MI', 'MN', 'MS',
-      'MO', 'MT', 'NE', 'NV',
-      'NH', 'NJ', 'NM', 'NY',
-      'NC', 'ND', 'OH', 'OK',
-      'PA', 'RI', 'SC', 'SD',
-      'TN', 'TX', 'UT', 'VT',
-      'VA', 'WA', 'WV', 'WI', 
-      'WY', 'DC', 'AS', 'GU', 
-      'MP', 'PR', 'UM', 'VI' 
+      'AL', 'AK', 
+      'AZ', 'AR', 
+      'CA', 'CO', 
+      'CT', 'DE', 
+      'FL', 'GA', 
+      'HI', 'ID',
+      'IL', 'IN', 
+      'IA', 'KS',
+      'KY', 'LA', 
+      'ME', 'MD',
+      'MA', 'MI', 
+      'MN', 'MS',
+      'MO', 'MT', 
+      'NE', 'NV',
+      'NH', 'NJ', 
+      'NM', 'NY',
+      'NC', 'ND', 
+      'OH', 'OK',
+      'PA', 'RI', 
+      'SC', 'SD',
+      'TN', 'TX', 
+      'UT', 'VT',
+      'VA', 'WA', 
+      'WV', 'WI', 
+      'WY', 'DC', 
+      'AS', 'GU', 
+      'MP', 'PR', 
+      'VI' 
     ]);
 
-    this.stateMap = new Map<string,string>([['Alabama','AL'],['Arkansas','AK']]);
-    for(var state of this.twoLetterStates) {
-
-    }
-
-    // console.log(this.states.size);
+    this.stateMap = new Map<string,string>([
+      ['Alabama','AL'],['Alaska','AK'],
+      ['Arizona','AZ'],['Arkansas','AK'],
+      ['California','CA'],['Colorado','CO'],
+      ['Connecticut','CT'],['Delaware','DE'],
+      ['Florida','FL'],['Georgia','GA'],
+      ['Hawaii','HI'],['Idaho','ID'],
+      ['Illinois','IL'],['Indiana','IN'],
+      ['Iowa','IA'],['Kansas','KS'],
+      ['Kentucky','KY'],['Louisiana','LA'],
+      ['Maine','ME'],['Maryland','MD'],
+      ['Massachusetts','MA'],['Michigan','MI'],
+      ['Minnesota','MN'],['Mississippi','MS'],
+      ['Missouri','MO'],['Montana','MT'],
+      ['Nebraska','NE'],['Nevada','NV'],
+      ['New Hampshire','NH'],['New Jersey','NJ'],
+      ['New Mexico','NM'],['New York','NY'],
+      ['North Carolina','NC'],['North Dakota','ND'],
+      ['Ohio','OH'],['Oklahoma','OK'],
+      ['Pennsylvania','PA'],['Rhode Island','RI'],
+      ['South Carolina','SC'],['South Dakota','SD'],
+      ['Tennessee','TN'],['Texas','TX'],
+      ['Utah','UT'],['Vermont','VT'],
+      ['Virginia','VA'],['Washington','WA'],
+      ['West Virginia','WV'],['Wisconsin','WI'],
+      ['Wyoming','WI'],['District of Columbia', 'DC'],
+      ['American Samoa','AS'],['Guam','GU'],
+      ['Northern Mariana Islands','MP'],['Puerto Rico','PR'],
+      ['United States Virgin Islands','VI']
+    ]);
+    
+    console.log(this.twoLetterStates.size +  " " + this.stateMap.size);
   }
 
   isUSState(value: string) : boolean {
